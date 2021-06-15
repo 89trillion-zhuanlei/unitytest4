f9d7eea3b391cf43f47bf6da8643d9bae8e986ac
89trillion-zhuanlei <anlei@89trillion.com>
2021-06-15 11:11:20 +0800

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Animator playerAnim;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerAnim.SetBool("Idle",false);
+            playerAnim.SetBool("Attack",true);
+        }else if (Input.GetKeyDown(KeyCode.I))
+        {
+            playerAnim.SetBool("Idle",true);
+        }else if (Input.GetKey(KeyCode.R))
+        {
+            playerAnim.SetBool("Idle",false);
+            playerAnim.SetBool("Run",true);
+        }
+        else
+        {
+            playerAnim.SetBool("Run",false);
+            playerAnim.SetBool("Attack",false);
+        }
+    }
+}
